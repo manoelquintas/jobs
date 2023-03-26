5308a910f286abfb861972376b00a634f8a2678c
Francisco Quintas <47667099+manoelquintas@users.noreply.github.com>
2023-03-26 16:45:02 -0300

    Initial commit

---
+++ src/main/java/br/com/expedit/jobs/JobsApplication.java
@@ -0,0 +1,13 @@
+package br.com.expedit.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsApplication.class, args);
+	}
+
+}
